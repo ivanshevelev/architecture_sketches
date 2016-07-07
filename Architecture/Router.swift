2c596478ba578554016fb00fdad9a4819b918b58
Ivan Shevelev <ivan.shevelev@sibers.com>
2016-07-07 23:32:47 +0600

    add viewcontroller swizzling for perform segue with config closure and router base class

---
+++ Architecture/Router.swift
@@ -0,0 +1,45 @@
+//
+//  Router.swift
+//  Architecture
+//
+//  Created by Ivan Shevelev on 25/05/16.
+//  Copyright © 2016 Sibers. All rights reserved.
+//
+
+import UIKit
+
+class Router<T: UIViewController> {
+    
+    weak private(set) var viewController: T?
+    
+    init(viewController: T) {
+        self.viewController = viewController
+    }
+    
+    func performSegue<DestinationViewControllerType>(identifier identifier: String, configurate: ((viewController: DestinationViewControllerType) -> Void)?) {
+        
+        self.viewController?.performSegueWithIdentifier(identifier, sender: self) { (segue) in
+            
+            if let navigationViewController = segue.destinationViewController as? UINavigationController {
+                if let rootViewController = navigationViewController.viewControllers.first {
+                    self.perform(viewController: rootViewController, byIdentifier: identifier, configurate: configurate)
+                } else {
+                    fatalError("segue with identifier: \(identifier) has destinationViewController as UINavigationController without viewControllers")
+                }
+            } else {
+                self.perform(viewController: segue.destinationViewController, byIdentifier: identifier, configurate: configurate)
+            }
+            
+        }
+        
+    }
+    
+    private func perform<DestinationViewControllerType>(viewController viewController: UIViewController, byIdentifier identifier: String, configurate: ((viewController: DestinationViewControllerType) -> Void)?) {
+        guard let viewController = viewController as? DestinationViewControllerType else {
+            fatalError("segue with identifier: \(identifier) doesn't has destinationViewController with type \(DestinationViewControllerType.self)")
+        }
+        
+        configurate?(viewController: viewController)
+    }
+    
+}
---
+++ Architecture/ViewControllersRoutingSwizzle.swift
@@ -0,0 +1,70 @@
+//
+//  ViewControllersRoutingSwizzle.swift
+//  Architecture
+//
+//  Created by Ivan Shevelev on 01/06/16.
+//  Copyright © 2016 Sibers. All rights reserved.
+//
+
+import UIKit
+
+typealias ConfiguratePerformSegue = (UIStoryboardSegue) -> ()
+
+class ConfigurateClosureWrapper {
+    var value: ConfiguratePerformSegue?
+    required init(value: ConfiguratePerformSegue?) {
+        self.value = value
+    }
+}
+
+extension UIViewController {
+
+    struct AssociatedKey {
+        static var ClosurePrepareForSegueKey = "ClosurePrepareForSegueKey"
+        static var token: dispatch_once_t = 0
+    }
+    
+    typealias ConfiguratePerformSegue = (UIStoryboardSegue) -> ()
+    func performSegueWithIdentifier(identifier: String, sender: AnyObject?, configurate: ConfiguratePerformSegue?) {
+        swizzlingPrepareForSegue()
+        self.configuratePerformSegue = configurate
+        performSegueWithIdentifier(identifier, sender: sender)
+    }
+    
+    private func swizzlingPrepareForSegue() {
+        dispatch_once(&AssociatedKey.token) {
+            let originalSelector = #selector(UIViewController.prepareForSegue(_:sender:))
+            let swizzledSelector = #selector(UIViewController.closurePrepareForSegue(_:sender:))
+            
+            let instanceClass = UIViewController.self
+            let originalMethod = class_getInstanceMethod(instanceClass, originalSelector)
+            let swizzledMethod = class_getInstanceMethod(instanceClass, swizzledSelector)
+            
+            let didAddMethod = class_addMethod(instanceClass, originalSelector,
+                                               method_getImplementation(swizzledMethod), method_getTypeEncoding(swizzledMethod))
+            
+            if didAddMethod {
+                class_replaceMethod(instanceClass, swizzledSelector,
+                                    method_getImplementation(originalMethod), method_getTypeEncoding(originalMethod))
+            } else {
+                method_exchangeImplementations(originalMethod, swizzledMethod)
+            }
+        }
+    }
+    
+    func closurePrepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        self.configuratePerformSegue?(segue)
+        self.closurePrepareForSegue(segue, sender: sender)
+        self.configuratePerformSegue = nil
+    }
+    
+    var configuratePerformSegue: ConfiguratePerformSegue? {
+        get {
+            let box = objc_getAssociatedObject(self, &AssociatedKey.ClosurePrepareForSegueKey) as? ConfigurateClosureWrapper
+            return box?.value
+        }
+        set {
+            objc_setAssociatedObject(self, &AssociatedKey.ClosurePrepareForSegueKey, ConfigurateClosureWrapper(value: newValue), objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN)
+        }
+    }
+}
